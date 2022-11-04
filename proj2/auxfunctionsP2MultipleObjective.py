a5f979b96de5c9583d6591d647954b7cbb488331
Resende810 <pedrofilipe.pintoresende@gmail.com>
2022-11-04 21:39:19 +0000

    Asfasfasfa

--- proj2/auxfunctionsP2MultipleObjective.py
+++ proj2/auxfunctionsP2MultipleObjective.py
@@ -2,73 +2,14 @@
 import matplotlib.pyplot as plt
 
 
-
-def MO_evaluatecostfixed(individual, CustDist, CustOrd_list):
-    total_distance = 0 
-    load = 1000 
-    total_cost = 0    
-
-
-
-    for i in range(len(individual)):
-        individual[i] = individual[i] + 1
-        
-    
-    for i in range(len(individual)):
-        #doesnt have enough load 
-        if(load < 50):
-            #last location -> central
-            total_distance = total_distance + CustDist[0][individual[i-1]]
-            
-            # calcula o custo de ir à central com o camião com a current load 
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
-            load = 1000
-            
-            
-            #central -> next location
-            total_distance = total_distance + CustDist[0][individual[i]]
-            # calcula o custo de ir da central para o próximo cliente (custo = carga * distância)
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
-            load = load - 50
-            
-        
-        #last client
-        if(i == (len(individual) - 1)):
-            #last location -> next location
-            total_distance = total_distance + CustDist[individual[i]][individual[i-1]]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
-            load = load - 50
-            #next location -> warehouse
-            total_distance = total_distance + CustDist[individual[i]][0]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
-        
-        #first client
-        if(i == 0):
-            #warehouse -> next location
-            total_distance = total_distance + CustDist[0][individual[i]]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
-            load = load - 50
-        
-        #has enough load 
-        else: 
-            #last location -> next location
-            total_distance = total_distance + CustDist[individual[i]][individual[i-1]]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
-            load = load - 50
-            
-        for i in range(len(individual)):
-            individual[i] = individual[i] - 1
-            
-    return total_distance, total_cost,
-
-
-
 def MO_evaluatecostvariable(individual, CustDist, CustOrd_list):
     total_distance = 0 
     load = 1000 
     total_cost = 0
+    
     for i in range(len(individual)):
         individual[i] = individual[i] + 1
+    
     
     #falta ele ir ter de descarregar na última cena (corrigir)
     for i in range(len(individual)):
@@ -81,7 +22,7 @@ def MO_evaluatecostvariable(individual, CustDist, CustOrd_list):
             
             #central -> next location
             total_distance = total_distance + CustDist[0][individual[i]]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
+            total_cost = total_cost + load * CustDist[0][individual[i]]
             load = load - CustOrd_list[individual[i]][0]
             
         
@@ -89,29 +30,31 @@ def MO_evaluatecostvariable(individual, CustDist, CustOrd_list):
         if(i == (len(individual) - 1)):
             #last location -> next location
             total_distance = total_distance + CustDist[individual[i]][individual[i-1]]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
+            total_cost = total_cost + load * CustDist[individual[i]][individual[i-1]]
             load = load - CustOrd_list[individual[i]][0]
             #next location -> warehouse
             total_distance = total_distance + CustDist[individual[i]][0]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
+            total_cost = total_cost + load * CustDist[individual[i]][0]
         
         #first client
         if(i == 0):
             #warehouse -> next location
             total_distance = total_distance + CustDist[0][individual[i]]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
+            total_cost = total_cost + load * CustDist[0][individual[i]]
             load = load - CustOrd_list[individual[i]][0]
         
         #has enough load 
         else: 
             #last location -> next location
             total_distance = total_distance + CustDist[individual[i]][individual[i-1]]
-            total_cost = total_cost + load * CustDist[0][individual[i-1]]
+            total_cost = total_cost + load * CustDist[individual[i]][individual[i-1]]
             load = load - CustOrd_list[individual[i]][0]
+        
+        
+    for i in range(len(individual)):
+        individual[i] = individual[i] - 1
             
-        for i in range(len(individual)):
-            individual[i] = individual[i] - 1
-            
+        
     return total_distance, total_cost,
 
 
@@ -132,9 +75,12 @@ def plot_pareto_frontier(Xs, Ys, maxX=True, maxY=True):
     pf_X = [pair[0] for pair in pareto_front]
     pf_Y = [pair[1] for pair in pareto_front]
     plt.plot(pf_X, pf_Y)
-    plt.xlabel("Cost")
-    plt.ylabel("Distance") 
+    plt.xlabel("Distance")
+    plt.ylabel("Cost") 
 
     # higlight the minimum cost and minimum distance solutions
 
     plt.show()
+    
+    return pareto_front
+    
